714de2fc14af5e029f1b7b82898988788566d385
flavio2018 <flavio.petruzzellis@gmail.com>
2022-02-24 18:22:57 +0100

    Add sample test for DNTM

--- src/models/test_DynamicNeuralTuringMachine.py
+++ src/models/test_DynamicNeuralTuringMachine.py
@@ -1,8 +1,17 @@
-import unittest
+# from ward import test
+import torch
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from src.models.DynamicNeuralTuringMachine import DynamicNeuralTuringMachine, DynamicNeuralTuringMachineMemory
 
-if __name__ == '__main__':
-    unittest.main()
+
+# @test("Test DNTM")
+def test_dntm_memory_content_shape():
+    n_locations = 100
+    content_size = 100
+    address_size = 20
+    example_hidden_state = torch.randn((n_locations, 1))
+    dntm_memory = DynamicNeuralTuringMachineMemory(n_locations=n_locations, content_size=content_size,
+                                                   address_size=address_size, controller_input_size=100)
+    dntm_memory.address_memory(example_hidden_state)
+    memory_contents = dntm_memory.read()
+    assert memory_contents.shape == (content_size+address_size, 1)
