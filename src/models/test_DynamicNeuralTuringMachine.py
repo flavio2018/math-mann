8aff20475d22b78fc9e7f31ad7fb8fb22e772f81
flavio2018 <flavio.petruzzellis@gmail.com>
2022-02-25 17:20:00 +0100

    Add some fixtures

--- src/models/test_DynamicNeuralTuringMachine.py
+++ src/models/test_DynamicNeuralTuringMachine.py
@@ -15,12 +15,22 @@ def _init_dntm_memory_parameters():
     }
 
 
+def _mock_controller_input():
+    controller_input_size = 100
+    return torch.ones((controller_input_size, 1))
+
+
+def _mock_controller_hidden_state():
+    memory_parameters = _init_dntm_memory_parameters()
+    return torch.randn((memory_parameters["n_locations"], 1))
+
+
 def test_dntm_memory_content_shape():
     memory_parameters = _init_dntm_memory_parameters()
-    example_hidden_state = torch.randn((memory_parameters["n_locations"], 1))
+    mock_hidden_state = _mock_controller_hidden_state()
     dntm_memory = DynamicNeuralTuringMachineMemory(
         **memory_parameters, controller_input_size=100)
-    dntm_memory.address_memory(example_hidden_state)
+    dntm_memory.address_memory(mock_hidden_state)
     memory_contents = dntm_memory.read()
     assert memory_contents.shape == (
         memory_parameters["content_size"] + memory_parameters["address_size"], 1)
@@ -28,18 +38,18 @@ def test_dntm_memory_content_shape():
 
 def test_dntm_memory_address_vector_shape():
     memory_parameters = _init_dntm_memory_parameters()
-    example_hidden_state = torch.randn((memory_parameters["n_locations"], 1))
+    mock_hidden_state = _mock_controller_hidden_state()
     dntm_memory = DynamicNeuralTuringMachineMemory(
         **memory_parameters, controller_input_size=100)
-    dntm_memory.address_memory(example_hidden_state)
+    dntm_memory.address_memory(mock_hidden_state)
     assert dntm_memory.address_vector.shape == (
         memory_parameters["n_locations"], 1)
 
 
 def test_dntm_memory_address_vector_sum_to_one():
     memory_parameters = _init_dntm_memory_parameters()
-    example_hidden_state = torch.randn((memory_parameters["n_locations"], 1))
+    mock_hidden_state = _mock_controller_hidden_state()
     dntm_memory = DynamicNeuralTuringMachineMemory(
         **memory_parameters, controller_input_size=100)
-    dntm_memory.address_memory(example_hidden_state)
+    dntm_memory.address_memory(mock_hidden_state)
     assert dntm_memory.address_vector.sum().item() == pytest.approx(1)
