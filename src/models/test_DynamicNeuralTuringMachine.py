a2ba46b3be5cb02ff7c59cee5365d996677760f3
flavio2018 <flavio.petruzzellis@gmail.com>
2022-02-25 15:43:52 +0100

    Fix test on address vector shape

--- src/models/test_DynamicNeuralTuringMachine.py
+++ src/models/test_DynamicNeuralTuringMachine.py
@@ -1,17 +1,35 @@
-# from ward import test
 import torch
 
 from src.models.DynamicNeuralTuringMachine import DynamicNeuralTuringMachine, DynamicNeuralTuringMachineMemory
 
 
-# @test("Test DNTM")
-def test_dntm_memory_content_shape():
+def _init_dntm_memory_parameters():
     n_locations = 100
     content_size = 100
     address_size = 20
-    example_hidden_state = torch.randn((n_locations, 1))
-    dntm_memory = DynamicNeuralTuringMachineMemory(n_locations=n_locations, content_size=content_size,
-                                                   address_size=address_size, controller_input_size=100)
+    return {
+        "n_locations": n_locations,
+        "content_size": content_size,
+        "address_size": address_size,
+    }
+
+
+def test_dntm_memory_content_shape():
+    memory_parameters = _init_dntm_memory_parameters()
+    example_hidden_state = torch.randn((memory_parameters["n_locations"], 1))
+    dntm_memory = DynamicNeuralTuringMachineMemory(
+        **memory_parameters, controller_input_size=100)
     dntm_memory.address_memory(example_hidden_state)
     memory_contents = dntm_memory.read()
-    assert memory_contents.shape == (content_size+address_size, 1)
+    assert memory_contents.shape == (
+        memory_parameters["content_size"] + memory_parameters["address_size"], 1)
+
+
+def test_dntm_memory_address_vector_shape():
+    memory_parameters = _init_dntm_memory_parameters()
+    example_hidden_state = torch.randn((memory_parameters["n_locations"], 1))
+    dntm_memory = DynamicNeuralTuringMachineMemory(
+        **memory_parameters, controller_input_size=100)
+    dntm_memory.address_memory(example_hidden_state)
+    assert dntm_memory.address_vector.shape == (
+        memory_parameters["n_locations"], 1)
