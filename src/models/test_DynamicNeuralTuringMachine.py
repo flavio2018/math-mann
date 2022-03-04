239cc6c8755d0a5b3fb8d5619b10ff85c35faf48
flavio2018 <flavio.petruzzellis@gmail.com>
2022-03-04 16:33:51 +0100

    Add test for nan in address vec

--- src/models/test_DynamicNeuralTuringMachine.py
+++ src/models/test_DynamicNeuralTuringMachine.py
@@ -51,6 +51,16 @@ def test_dntm_memory_address_vector_shape():
         memory_parameters["n_locations"], 1)
 
 
+def test_dntm_memory_address_vector_contains_no_nan_values():
+    memory_parameters = _init_dntm_memory_parameters()
+    mock_hidden_state = _mock_controller_hidden_state()
+    dntm_memory = DynamicNeuralTuringMachineMemory(
+        **memory_parameters, controller_input_size=CONTROLLER_INPUT_SIZE)
+    with torch.no_grad():
+        dntm_memory.address_memory(mock_hidden_state)
+    assert not dntm_memory.address_vector.isnan().any()
+
+
 def test_dntm_memory_address_vector_sum_to_one():
     memory_parameters = _init_dntm_memory_parameters()
     mock_hidden_state = _mock_controller_hidden_state()
