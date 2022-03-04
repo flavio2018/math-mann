a63e2f3f2fa2c3203494bde5d66d5d4048782ee7
flavio2018 <flavio.petruzzellis@gmail.com>
2022-03-04 16:31:49 +0100

    Add logging printouts

--- src/models/train_dntm_pmnist.py
+++ src/models/train_dntm_pmnist.py
@@ -12,12 +12,16 @@ from torchvision import datasets
 from torchvision.transforms import Lambda
 from torch.utils.data import DataLoader
 
+
 from src.models.DynamicNeuralTuringMachine import DynamicNeuralTuringMachine, DynamicNeuralTuringMachineMemory
 
 
 @click.command()
+@click.option("--loglevel", type=str, default="INFO")
 @Timer(text=lambda secs: f"Took {format_timespan(secs)}")
-def main():
+def main(loglevel):
+    configure_logging(loglevel)
+
     def _convert_to_float32(x: np.array):
         return x.astype(np.float32)
 
@@ -56,15 +60,56 @@ def main():
     dntm = DynamicNeuralTuringMachine(
         memory=dntm_memory,
         controller_hidden_state_size=n_locations,
-        controller_input_size=controller_input_size
-    )
+        controller_input_size=controller_input_size,
+        controller_output_size=controller_output_size
+    ).to("cuda")
 
-    for epoch in range(1):
-        for sample, target in data_loader:
-            for pixel in sample[0]:
-                dntm(pixel.view(1, 1))
+    loss_fn = torch.nn.NLLLoss()
+    opt = torch.optim.Adam(dntm.parameters())
+
+    # TODO handle minibatches?
+    torch.autograd.set_detect_anomaly(True)
+    for epoch in range(2):
+        for i, (mnist_image, target) in enumerate(data_loader):
+            logging.info(f"MNIST image {i}")
+            dntm.zero_grad()
+
+            logging.debug(f"Moving image to GPU")
+            mnist_image, target = mnist_image.to("cuda"), target.to("cuda")
+
+            logging.debug(f"Looping through image pixels")
+            for pixel in mnist_image[0]:
+                __, output = dntm(pixel.view(1, 1))
+
+            logging.debug(f"Computing loss value")
+            loss_value = loss_fn(output.T, target)
+
+            logging.debug(f"Computing gradients")
+            loss_value.backward()
+
+            logging.debug(f"Running optimization step")
+            opt.step()
+
+            logging.debug(f"Resetting the memory")
             dntm.memory.reset_memory_content()
-            break
+
+        logging.info(f"{epoch=}: {loss_value=}")
+
+
+def configure_logging(loglevel):
+    # assuming loglevel is bound to the string value obtained from the
+    # command line argument. Convert to upper case to allow the user to
+    # specify --log=DEBUG or --log=debug
+    numeric_level = getattr(logging, loglevel.upper(), None)
+
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+
+    logging.basicConfig(
+        filename='train_dntm_pmnist.log',
+        level=numeric_level,
+        format='%(levelname)s:%(message)s',
+        filemode="w")
 
 
 if __name__ == "__main__":
