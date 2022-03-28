808ef5465bdc6cae6b28b047b7dd724fc66b40e3
flavio2018 <flavio.petruzzellis@gmail.com>
2022-03-28 18:06:50 +0200

    Implement regularization term

---
+++ src/models/train_dntm_utils.py
@@ -0,0 +1,18 @@
+import torch
+
+
+def read_write_consistency_regularizer(sequence_read_weights, sequence_write_weights, lambda_):
+    """This method implements the first regularization term described in the paper.
+    The regularization term uses all the read and write weights computed by the model
+    processing a sequence, which should therefore be collected during training.
+
+    The method assumes that read and write weights are column vectors, i.e. they have shape (1, N)
+    and the batches of weights have shapes (k, N), where N is the number of memory locations and k
+    is the sequence length."""
+    term = torch.zeros(sequence_read_weights.shape[0])
+    for t in range(sequence_read_weights.shape[0]):
+        normalized_sum_of_write_weights_up_to_t = sequence_write_weights[:t+1, :].sum(axis=0).view(1, -1) / (t+1)
+        scaled_product_with_read_weights = 1 - normalized_sum_of_write_weights_up_to_t.T @ sequence_read_weights[t, :].view(1, -1)
+        norm = torch.linalg.matrix_norm(scaled_product_with_read_weights)
+        term[t] = norm**2
+    return lambda_ * term.sum()
