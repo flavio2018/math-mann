d5dbc7a3cda886413a8f0322dedff32934430514
flavio2018 <flavio.petruzzellis@gmail.com>
2022-05-02 17:32:45 +0200

    Add MLP

--- src/models/train_dntm_utils.py
+++ src/models/train_dntm_utils.py
@@ -24,6 +24,8 @@ def read_write_consistency_regularizer(sequence_read_weights, sequence_write_wei
 def build_model(model_conf, device):
     if model_conf.name == 'dntm':
         return build_dntm(model_conf, device)
+    elif model_conf.name == 'mlp':
+        return build_mlp(model_conf, device)
     else:
         return build_rnn(model_conf, device)
 
@@ -78,3 +80,37 @@ def build_rnn(model_conf, device):
                       hidden_size=model_conf.hidden_size,
                       proj_size=model_conf.output_size,
                       device=device)
+
+
+class CustomMLP(torch.nn.Module):
+    def __init__(self, input_size: int, hidden_sizes: str, output_size: int, device):
+        """hidden_sizes should be a list of comma separated integers."""
+        hidden_sizes = [int(size) for size in hidden_sizes.split(",")]
+        super().__init__()
+        hidden_sizes = [input_size] + hidden_sizes
+        self.linear_layers = []
+        for i in range(len(hidden_sizes) - 1):
+            self.linear_layers += [torch.nn.Linear(in_features=hidden_sizes[i],
+                                                   out_features=hidden_sizes[i+1],
+                                                   device=device)]
+        self.output_layer = torch.nn.Linear(in_features=hidden_sizes[-1], out_features=output_size, device=device)
+
+    def forward(self, x):
+        x = x.view(-1, 784)
+        relu = torch.nn.ReLU()
+
+        for layer in self.linear_layers:
+            x = layer(x)
+            x = relu(x)
+        x = self.output_layer(x).T  # output shape is (-1, output_size)
+        return None, torch.nn.functional.log_softmax(x, dim=0)
+
+    def prepare_for_batch(self, batch, device):
+        return
+
+
+def build_mlp(model_conf, device):
+    return CustomMLP(input_size=model_conf.input_size,
+                     hidden_sizes=model_conf.hidden_sizes,
+                     output_size=model_conf.output_size,
+                     device=device)
