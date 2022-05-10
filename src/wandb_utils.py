d05398b6f35237b83ef983d903d8d8c1203b1981
flavio2018 <flavio.petruzzellis@gmail.com>
2022-05-10 19:04:37 +0200

    Add global norm of gradients logging

--- src/wandb_utils.py
+++ src/wandb_utils.py
@@ -11,7 +11,16 @@ def log_weights_gradient(model):
             wandb.log({f"norm2_gradient/{param_name}": norm})
         else:
             logging.warning(f"{param_name} gradient is None!")
-
+    # code adapted from https://pytorch.org/docs/stable/_modules/torch/nn/utils/clip_grad.html#clip_grad_norm_
+    norm_type=2
+    parameters = model.parameters()
+    if isinstance(parameters, torch.Tensor):
+        parameters = [parameters]
+    parameters = [p for p in parameters if p.grad is not None]
+    norm_type = float(norm_type)
+    device = parameters[0].grad.device
+    total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), norm_type).to(device) for p in parameters]), norm_type)
+    wandb.log({f"norm2_gradient/global": total_norm})
 
 def log_preds_and_targets(batch_i, output, targets):
     if batch_i == 0:
