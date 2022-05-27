958565e5f65ec7f5fac5e8678d01dccbb413e05d
flavio2018 <flavio.petruzzellis@gmail.com>
2022-05-27 17:42:02 +0200

    Update math train script
    - change loss computation to include masking and
      re-initialization of hidden states
    - unify train and valid step in single function

--- src/models/train_maths.py
+++ src/models/train_maths.py
@@ -41,7 +41,7 @@ def train_and_test_dntm_maths(cfg):
 
     model = build_model(cfg.model, device)
 
-    criterion = torch.nn.NLLLoss()
+    criterion = torch.nn.NLLLoss(reduction='none')
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.train.lr, betas=(0.99, 0.995), eps=1e-9)
     early_stopping = EarlyStopping(verbose=True,
                                    path=os.path.join(os.getcwd(),
@@ -50,8 +50,10 @@ def train_and_test_dntm_maths(cfg):
                                    patience=cfg.train.patience)
 
     for epoch in range(cfg.train.epochs):
-        train_loss, train_accuracy, cer, mer = train_step(train_dataloader, vocab, model, criterion, optimizer, device, text_table, cfg, epoch)
-        valid_loss, valid_accuracy = valid_step(valid_dataloader, model, criterion, device, vocab)
+        train_loss, train_accuracy, cer, mer = step_on_dataset(train_dataloader, model, criterion, optimizer,
+                                                               device, vocab, text_table, cfg, epoch, train=True)
+        valid_loss, valid_accuracy, valid_cer, valid_mer = step_on_dataset(train_dataloader, model, criterion, optimizer,
+                                                                           device, vocab, text_table, cfg, epoch, train=False)
 
         print(f"Epoch {epoch} --- train loss: {train_loss} - train acc: {train_accuracy} - "
               f"valid loss: {valid_loss} - valid acc: {valid_accuracy}")
@@ -71,75 +73,77 @@ def train_and_test_dntm_maths(cfg):
     wandb.log({'predictions': text_table})
 
 
-def train_step(data_loader, vocab, model, criterion, optimizer, device, text_table, cfg, epoch):
+def step_on_dataset(dataloader, model, criterion, optimizer,
+    device, vocab, text_table, cfg, epoch, train: bool):
     char_error_rate = CharErrorRate().to(device)
     match_error_rate = MatchErrorRate().to(device)
     global_accuracy = 0
-    num_samples = 0
-
     epoch_loss = 0
+    num_samples = 0
     num_batches = 0
-    model.train()
-    for batch, targets in data_loader:
+    
+    if train:
+        model.train()
+    else:
+        model.eval()
+
+    for batch, targets, masks in dataloader:
         num_batches += 1
         batch_size = len(batch)
+        masks_X, masks_Y = masks
+        input_sequences_lengths = masks_X.sum(axis=1).to(device)
+        target_sequences_lengths = masks_Y.sum(axis=1).to(device)
         target_seq_len = targets.shape[1]  # targets.shape = (bs, target_seq_len)
         batch, targets = batch.to(device), targets.to(device)
         one_hot_batch = torch.nn.functional.one_hot(batch.type(torch.int64), len(vocab)).type(torch.float32)
         one_hot_targets = torch.nn.functional.one_hot(targets.type(torch.int64), len(vocab)).type(torch.float32)
         
-        model.zero_grad()
+        if train:
+            model.zero_grad()
+
         model.prepare_for_batch(one_hot_batch, device)
 
-        hn_cn, output = model(one_hot_batch)
-        # print(f"{batch.shape=}")
-        # print(f"{output.shape=}")
-        # print(f"{one_hot_targets.shape=}")
-        # print(f"{one_hot_batch.shape=}")
-        # print(f"{targets.shape=}")
-
+        hidden_states, outputs = model(one_hot_batch)
+        output = torch.stack([outputs[l-1,:,b] for l, b in zip(input_sequences_lengths, range(batch_size))]) 
+        
+        hidden_state = model.set_hidden_state(hidden_states, input_sequences_lengths, batch_size)
         current_output = output
+        predictions = [current_output.T]
         
         current_outputs_str = []
         current_outputs_str = update_outputs_str(current_outputs_str, current_output, vocab)
         
-        batch_loss = criterion(current_output.T, targets[:, 0].type(torch.int64))
-        # print("generating output")
-        # print(f"{current_output.T.shape=}")
-        # print(f"{batch_loss=}")
         for target_seq_pos in range(1, target_seq_len):
             target_seq_el = targets[:, target_seq_pos].type(torch.int64)  # target_seq_el.shape = (bs, 1)
             target_seq_el_1hot = one_hot_targets[:, target_seq_pos, :]
             new_input = target_seq_el_1hot.unsqueeze(dim=1)
             hn_cn, current_output = model(new_input)
-            batch_loss += criterion(current_output.T, target_seq_el)
+            current_output = current_output.reshape(current_output.shape[1:])
+            predictions.append(current_output)
             current_outputs_str = update_outputs_str(current_outputs_str, current_output, vocab)
-    
+        
+        predictions = torch.stack(predictions).reshape(batch_size, len(vocab), target_seq_len)
+        batch_loss_2d = criterion(predictions, targets.type(torch.int64))
+        batch_loss_2d = torch.where(masks_Y.to(device), batch_loss_2d, torch.zeros_like(batch_loss_2d))
+        batch_loss = torch.mean(batch_loss_2d.sum(axis=1) / target_sequences_lengths)
+        
         current_targets_str = []
         for target in targets:
             current_targets_str.append(''.join([vocab.lookup_token(t) for t in target]))
             
-        #print(current_targets_str, current_outputs_str)
-        if (epoch % 3 == 0) and (num_batches % 100 == 0):
+        if train and (epoch % 3 == 0) and (num_batches % 2 == 0):
             text_table.add_data(epoch, current_outputs_str[0], current_targets_str[0])
         correct_in_batch, num_samples_in_batch = num_correct_in_batch(current_outputs_str, current_targets_str)
         global_accuracy += correct_in_batch
         num_samples += num_samples_in_batch
         char_error_rate.update(current_outputs_str, current_targets_str)
         match_error_rate.update(current_outputs_str, current_targets_str)
-
-        # print(f"{target_seq_el=}")
-        # print(f"{new_input.shape=}")
-        # print(f"{current_output.T.shape=}")
-        # print(f"{batch_loss=}")
-        # print(f"{current_output.T=}")
-        # print(f"{new_input=}")
-
-        # print()
         epoch_loss += batch_loss.item() * batch_size
-        batch_loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), cfg.train.max_grad_norm, norm_type=2.0, error_if_nonfinite=True)
-        optimizer.step()
+
+        if train:
+            batch_loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), cfg.train.max_grad_norm, norm_type=2.0, error_if_nonfinite=True)
+            optimizer.step()
     global_accuracy /= num_samples
     cer_over_batches = char_error_rate.compute()
     mer_over_batches = match_error_rate.compute()
@@ -147,52 +151,9 @@ def train_step(data_loader, vocab, model, criterion, optimizer, device, text_tab
     return epoch_loss, global_accuracy, cer_over_batches, mer_over_batches
 
 
-def valid_step(valid_dataloader, model, criterion, device, vocab):
-    valid_epoch_loss = 0
-    global_accuracy = 0
-    num_samples = 0
-
-    model.eval()
-    num_batches = 0
-    for batch, targets in valid_dataloader:
-        num_batches += 1
-        batch_size = len(batch)
-        target_seq_len = targets.shape[1]
-        batch, targets = batch.to(device), targets.to(device)
-        one_hot_batch = torch.nn.functional.one_hot(batch.type(torch.int64), len(vocab)).type(torch.float32)
-        one_hot_targets = torch.nn.functional.one_hot(targets.type(torch.int64), len(vocab)).type(torch.float32)
-        model.prepare_for_batch(batch, device)
-        
-        hn_cn, output = model(one_hot_batch)
-
-        current_output = output
-        current_outputs_str = []
-        current_outputs_str = update_outputs_str(current_outputs_str, current_output, vocab)
-        
-        batch_loss = criterion(current_output.T, targets[:, 0].type(torch.int64))
-        for target_seq_pos in range(1, target_seq_len):
-            target_seq_el = targets[:, target_seq_pos].type(torch.int64)  # target_seq_el.shape = (bs, 1)
-            target_seq_el_1hot = one_hot_targets[:, target_seq_pos, :]
-            new_input = target_seq_el_1hot.unsqueeze(dim=1)
-            hn_cn, current_output = model(new_input)
-            batch_loss += criterion(current_output.T, target_seq_el)
-            current_outputs_str = update_outputs_str(current_outputs_str, current_output, vocab)
-
-        current_targets_str = []
-        for target in targets:
-            current_targets_str.append(''.join([vocab.lookup_token(t) for t in target]))
-        valid_epoch_loss += batch_loss.item() * batch_size
-        correct_in_batch, num_samples_in_batch = num_correct_in_batch(current_outputs_str, current_targets_str)
-        global_accuracy += correct_in_batch
-        num_samples += num_samples_in_batch
-    global_accuracy /= num_samples
-    valid_epoch_loss /= num_batches
-    return valid_epoch_loss, global_accuracy
-
-
 def update_outputs_str(outputs_str, current_output, vocab):
     if len(outputs_str) == 0:
-        for output in current_output.T:
+        for output in current_output:
             outputs_str.append(vocab.lookup_token(output.argmax()))
     else:
         for output_i, output in enumerate(current_output.T):
